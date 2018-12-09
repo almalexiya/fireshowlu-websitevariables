c4a537ab252a985ea5df07e54769c7084d96fd14
Anadorr <admin@anadorr.org.ua>
2018-12-09 09:04:41 +0100

    Create common-variables.js

---
+++ common-variables.js
@@ -0,0 +1,57 @@
+-
+    var commonPageVariables = {
+        en: {
+            lang: "en",
+            pageTitle: "Fireshow in Luxembourg for your wedding or corporate event",
+            pageKeywords: "fire show, fireshow, fire show Luxembourg, fireshow Luxembourg, led show, wedding show, wedding gift, wedding entertainment, corporate show",
+            pageDescription: "",
+            orderNow: "Order now",
+            close: "Close",
+            footer: {
+                copyright: "Copyright &copy; fireshow.lu 2016",
+                photogallery1Title: "Highlights from our shows",
+                photogallery2Title: "Highlights from our LED shows",
+                videoPromoTitle:"Fireshow promotional video:",
+                videoThronesTitle:'Fireshow "Game of Thrones":',
+                videoSparkleTitle:"Sparkle show:",
+                videoLedTitle:"LED show:",
+                videoCorporateTitle:"Corporate events:"
+            }
+        },
+        fr: {
+            lang: "fr",
+            pageTitle: "Le spectacle de feu au Luxembourg pour votre mariage ou événement d'entreprise",
+            pageKeywords: "spectacle de feu, spectacle, mariage, spectacle mariage, Luxembourg, fire show, fireshow, fire show Luxembourg, fireshow Luxembourg, led show, wedding show, wedding gift, wedding entertainment, corporate show",
+            pageDescription: "",
+            orderNow: "Commandez",
+            close: "Fermer",
+            footer: {
+                copyright: "Copyright &copy; fireshow.lu 2016",
+                photogallery1Title: "Faits saillants de nos spectacles",
+                photogallery2Title: "Points forts de nos spectacles à LED",
+                videoPromoTitle:"Le spectacle de feu vidéo:",
+                videoThronesTitle:'Le spectacle de feu "Game of Thrones":',
+                videoSparkleTitle:"Spectacle d'étincelles:",
+                videoLedTitle:"Le show de lumière:",
+                videoCorporateTitle:"Le show d'entreprise:"
+            }
+        },
+        de: {
+            lang: "de",
+            pageTitle: "",
+            pageKeywords: "fire show, fireshow, fire show Luxembourg, fireshow Luxembourg, led show, wedding show, wedding gift, wedding entertainment, corporate show",
+            pageDescription: "",
+            orderNow: "Bestellen",
+            close: "Schließen",
+            footer: {
+                copyright: "Copyright &copy; fireshow.lu 2016",
+                photogallery1Title: "Highlights from our shows",
+                photogallery2Title: "Highlights from our LED shows",
+                videoPromoTitle:"Fireshow promotional video:",
+                videoThronesTitle:'Fireshow "Game of Thrones":',
+                videoSparkleTitle:"Sparkle show:",
+                videoLedTitle:"LED show:",
+                videoCorporateTitle:"Corporate events:"
+            }
+        }
+    }
